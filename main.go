061f765100a83b75572ce38926e21b4043d772de
FuqingWang <574594544@qq.com>
2021-06-20 22:36:56 -0400

    go interface trial

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
+import "fmt"
+
+
+type englishBot struct{}
+type spanishBot struct{}
+
+type bot interface {
+	greeting() string
+}
+
+func (b englishBot) greeting() string{
+	return "Hello World"
+}
+
+func (b spanishBot) greeting() string{
+	return "Hola Worlda"
+}
+
+func printGreeting(b bot){
+	fmt.Println(b.greeting())
+}
+
 func main() {
 	// cards := newDeck()
 	// cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
-	cards.shuffle()
-	cards.print()
+	// cards := newDeckFromFile("my_cards")
+	// cards.shuffle()
+	// cards.print()
+	en := englishBot{}
+	es := spanishBot{}
+	printGreeting(en)
+	printGreeting(es)
 }
